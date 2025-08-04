578fba29ca198841b94ef91a93112eabc70c91ea
John Sourvinos <johnsourvinos@hotmail.com>
2025-08-04 07:00:43 +0300

    Adds two more security headers

--- API/Infrastructure/Middleware/SecurityHeadersMiddleware.cs
+++ API/Infrastructure/Middleware/SecurityHeadersMiddleware.cs
@@ -13,11 +13,15 @@ namespace API.Infrastructure.Middleware {
         }
 
         public async Task InvokeAsync(HttpContext context) {
-            context.Response.Headers.Add("Content-Security-Policy", new StringValues("font-src https://appsourvinos.com https://fonts.cdnfonts.com fonts.googleapis.com fonts.gstatic.com"));
-            context.Response.Headers.Add("X-Content-Type-Options", new StringValues("nosniff"));
-            context.Response.Headers.Add("X-Frame-Options", new StringValues("SAMEORIGIN"));
+            context.Response.Headers.Remove("Server");
+            context.Response.Headers.Remove("X-Powered-By");
+            context.Response.Headers.Add("X-XSS-Protection", new StringValues("1; mode=block"));
+            context.Response.Headers.Add("X-Frame-Options", new StringValues("DENY"));
             context.Response.Headers.Add("Referrer-Policy", new StringValues("no-referrer-when-downgrade"));
+            context.Response.Headers.Add("X-Content-Type-Options", new StringValues("nosniff"));
+            context.Response.Headers.Add("X-Permitted-Cross-Domain-Policies", new StringValues("none"));
             context.Response.Headers.Add("Permissions-Policy", "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()");
+            context.Response.Headers.Add("Content-Security-Policy", new StringValues("font-src https://appsourvinos.com https://fonts.cdnfonts.com fonts.googleapis.com fonts.gstatic.com"));
             await _next(context);
         }
     }
